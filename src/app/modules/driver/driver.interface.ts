2c1ddeb4e8d25c2a84820fd03012493ddc0c85da
Asifur Rahman <asifur.rahman4904@gmail.com>
2026-06-03 15:18:34 +0600

    Add driver interface and model for driver management

---
+++ src/app/modules/driver/driver.interface.ts
@@ -0,0 +1,9 @@
+import { Types } from 'mongoose'
+
+export interface IDriver {
+  user: Types.ObjectId
+  vehicleInfo: string
+  isApproved?: boolean
+  isOnline?: boolean
+  location: string
+}
---
+++ src/app/modules/driver/driver.model.ts
@@ -0,0 +1,34 @@
+import { model, Schema } from 'mongoose'
+import { IDriver } from './driver.interface'
+
+const driverSchema = new Schema<IDriver>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true
+    },
+    vehicleInfo: {
+      type: String,
+      required: true
+    },
+    isApproved: {
+      type: Boolean,
+      default: false
+    },
+    isOnline: {
+      type: Boolean,
+      default: false
+    },
+    location: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+const Driver = model('Driver', driverSchema)
+
+export default Driver
